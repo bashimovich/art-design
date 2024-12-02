1cc24b24720ab8cb0e7e0d6b8b951f5d4189c68c
bashimovich <yagshy.script@gmail.com>
2024-12-02 15:43:03 +0200

    section animation done

--- script.js
+++ script.js
@@ -32,6 +32,17 @@ gsap.to(".circle__img", {
     top: "0", 
     ease: "none", 
 });
+gsap.to(".section__one", {
+    scrollTrigger: {
+        trigger: ".section__five",
+        start: "top center",
+        end: "center top",
+        scrub: true, 
+    },
+    background:"#141414",
+    ease: "none", 
+});
+
 
 gsap.to(".square__img", {
     scrollTrigger: {
@@ -46,6 +57,62 @@ gsap.to(".square__img", {
         },
     },
 });
+gsap.to(".section__ten_img", {
+    scrollTrigger: {
+        trigger: ".section__ten",
+        start: "top center",
+        end: "center top",
+        toggleActions: "restart pause resume pause",
+        scrub: true, 
+        toggleClass: {
+            targets: ".section__ten_img",
+            className: "active",
+        },
+    },
+});
+
+gsap.to(".text__circle__img", {
+    scrollTrigger: {
+        trigger: ".section__ele",
+        start: "top center",
+        end: "center top",
+        toggleActions: "restart pause resume pause",
+        scrub: true, 
+        toggleClass: {
+            targets: ".text__circle__img",
+            className: "active",
+        },
+    },
+});
+
+gsap.to(".twe_img_1", {
+    scrollTrigger: {
+        trigger: ".section__twe",
+        start: "top center",
+        end: "center top",
+        toggleActions: "restart pause resume pause",
+        scrub: true, 
+        toggleClass: {
+            targets: ".twe_img_1",
+            className: "active",
+        },
+    },
+});
+
+gsap.to(".twe_img_2", {
+    scrollTrigger: {
+        trigger: ".section__twe",
+        start: "top center",
+        end: "center top",
+        toggleActions: "restart pause resume pause",
+        scrub: true, 
+        toggleClass: {
+            targets: ".twe_img_2",
+            className: "active",
+        },
+    },
+});
+
 gsap.to(".circle__img", {
     scrollTrigger: {
         trigger: ".section__three",
@@ -131,4 +198,74 @@ prev2.addEventListener('click', function(){
     document.querySelector('.slide2').prepend(items2[items2.length - 1])
 })
 
+
+
+
+ymaps.ready(init),
+	$(document).ready(function () {
+		$(document).mousemove(function (e) {
+			document.querySelector('.architecture__after-decor-left')
+			var t = $('.architecture__after-decor-left').offset(),
+				a = $('.architecture__after-decor-right').offset(),
+				o = t.left,
+				i = t.top,
+				r = a.left,
+				s = a.top,
+				n = e.pageX - o,
+				d = e.pageY - i,
+				l = e.pageX - r,
+				c = e.pageY - s
+			$('.architecture__after-decor-left').css({
+				transform: `translate(${n / 100}rem , ${d / 100}rem)`,
+			}),
+				$('.architecture__after-decor-right').css({
+					transform: `translate(${l / 100}rem , ${c / 100}rem)`,
+				})
+		})
+	})
+function init() {
+	var e = new ymaps.Map('map', {
+			center: [55.734851, 37.614455],
+			zoom: 15,
+			controls: ['zoomControl', 'typeSelector'],
+		}),
+		t = new ymaps.Placemark(
+			[55.734851, 37.614455],
+			{ hintContent: 'москва,ул. Большая Якиманка, 33/13с2 ' },
+			{
+				iconLayout: 'default#image',
+				iconImageHref:
+					'https://rgcompany.ru/images/tild3431-3737-4562-a163-643934666561__holder.svg',
+				iconImageSize: [24, 24],
+				iconImageOffset: [-16, -16],
+			}
+		)
+	jQuery(function (t) {
+		t(document).mouseup(function (a) {
+			t('.maps').is(a.target) || 0 !== t('.maps').has(a.target).length
+				? e.behaviors.enable('scrollZoom')
+				: e.behaviors.disable('scrollZoom')
+		})
+	}),
+		e.geoObjects.add(t)
+}
 // slider scripts end
+
+const cylinder_gallery = document.querySelector('.cylinder_gallery');
+const cylinder_images = document.querySelector('.cylinder_images');
+
+let mouseX = 0;
+let mouseY = 0;
+let rotationX = 0;
+let rotationY = 0;
+
+cylinder_gallery.addEventListener('mousemove', (e) => {
+    mouseX = e.clientX / window.innerWidth - 0.5;
+    mouseY = e.clientY / window.innerHeight - 0.5;
+    
+    rotationY = mouseX * 360;
+    rotationX = -mouseY * 180;
+    
+    cylinder_images.style.transform = `rotateX(${rotationX}deg) rotateY(${rotationY}deg)`;
+    cylinder_images.style.transition = `transform 2s`;
+});
